5b877a594a5c794f30deca1669efc20b46557348
Aditya Ojha <aditya.ojha2820@gmail.com>
2024-04-14 11:37:15 -0400

    updated html and index.js

--- frontend/index.js
+++ frontend/index.js
@@ -4,131 +4,117 @@ function searchPhoto() {
   var apigClient = apigClientFactory.newClient();
 
   var user_message = document.getElementById("note-textarea").value;
-  console.log(user_message);
-  var body = {};
+  console.log("Searching for:", user_message);
+
   var params = { q: user_message };
   var additionalParams = {
     headers: {
       "Content-Type": "application/json",
     },
   };
-  apigClient
-    .searchGet(params, body, additionalParams)
+
+  apigClient.searchGet(params, {}, additionalParams)
     .then(function (res) {
       var resp_data = res.data;
-      console.log(resp_data);
-      if (resp_data.imagePaths == "No Results found") {
-        document.getElementById("displaytext").innerHTML =
-          "Sorry could not find the image. Try another search words!";
+      console.log("Search response received:", resp_data);
+
+      if (resp_data.imagePaths === "No Results found") {
+        document.getElementById("displaytext").innerHTML = "Sorry could not find the image. Try another search words!";
         document.getElementById("displaytext").style.display = "block";
+      } else {
+        document.getElementById("img-container").innerHTML = "";
+        resp_data.imagePaths.forEach(function (obj) {
+          fetch(obj)
+            .then((response) => {
+              if (!response.ok) throw new Error("Network response was not ok");
+              return response.text();
+            })
+            .then((base64String) => {
+              const img = new Image();
+              img.src = `data:image/jpeg;base64,${base64String}`;
+              img.setAttribute("class", "banner-img");
+              img.setAttribute("alt", "Uploaded Image");
+              document.getElementById("img-container").appendChild(img);
+            })
+            .catch((error) => {
+              console.error("Error fetching the image from S3:", error);
+            });
+        });
       }
-
-      document.getElementById("img-container").innerHTML = "";
-
-      resp_data.imagePaths.forEach(function (obj) {
-        // Make a GET request to the S3 URL
-        fetch(obj)
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("Network response was not ok");
-            }
-            return response.text(); // Get the response as text
-          })
-          .then((base64String) => {
-            // Create an image element
-            const img = new Image();
-
-            // Set the source of the image to the decoded Base64 string
-            img.src = `data:image/jpeg;base64,${base64String}`;
-            img.setAttribute("class", "banner-img");
-            img.setAttribute("alt", "effy");
-
-            // Append the image to an HTML element with ID 'img-container'
-            document.getElementById("img-container").appendChild(img);
-          })
-          .catch((error) => {
-            console.error("There was a problem fetching the image:", error);
-          });
-      });
     })
-    .catch(function (result) {});
+    .catch(function (error) {
+      console.error("Error during search API call:", error);
+    });
+}
+
+function handleFileUpload(event) {
+  file = event.target.files[0];
+  console.log("File selected:", file.name);
+  document.querySelector(".upload_box header h4").innerText = file.name;
+}
+
+function submitPhotoAndLabel() {
+  const file_input = document.getElementById("file_input");
+  const custom_label = document.getElementById("custom_label").value;
+  const upload_button = document.querySelector(".upload_box button");
+
+  upload_button.innerText = "Uploading...";
+  upload_button.style.backgroundColor = "#005af0";
+
+  getBase64(file_input.files[0]).then((data) => {
+    var apigClient = apigClientFactory.newClient();
+    var body = data;
+    var params = {
+      object: file_input.files[0].name,
+      'x-amz-meta-customLabels': custom_label,
+      folder: "ass-b2"
+    };
+
+    var additionalParams = {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    };
+
+    console.log("Initiating upload with parameters:", params);
+
+    apigClient.uploadPut(params, body, additionalParams)
+      .then(function (res) {
+        console.log("Upload response:", res);
+        if (res.status === 200) {
+          upload_button.innerHTML = "Upload succeeded";
+          upload_button.style.backgroundColor = "#499C55";
+          console.log("Upload succeeded:", res.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Upload failed with error:", error);
+        upload_button.innerHTML = "Upload failed";
+        upload_button.style.backgroundColor = "#F54234";
+      });
+  }).catch((error) => {
+    console.error("Error preparing the file for upload:", error);
+    upload_button.innerHTML = "Upload failed";
+    upload_button.style.backgroundColor = "#F54234";
+  });
 }
 
 function getBase64(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    // reader.onload = () => resolve(reader.result)
     reader.onload = () => {
-      let encoded = reader.result.replace(/^data:(.*;base64,)?/, "");
+      let encoded = reader.result.replace(/^data:(.*;base64,)?/, '');
       if (encoded.length % 4 > 0) {
-        encoded += "=".repeat(4 - (encoded.length % 4));
+        encoded += '='.repeat(4 - encoded.length % 4);
       }
       resolve(encoded);
     };
-    reader.onerror = (error) => reject(error);
-  });
-}
-
-function uploadPhoto() {
-  //var file = document.getElementById('file_path').files[0];  // Ensure the file input element ID is correctly specified
-  console.log(file);
-  const reader = new FileReader();
-  document.getElementById("upload_button").innerHTML = "Uploading...";
-  document.getElementById("upload_button").style.backgroundColor = "#005af0";
-  getBase64(file).then((data) => {
-    var apigClient = apigClientFactory.newClient();
-    var note_customtag = document.getElementById("note_customtag").value; // Make sure to grab the value from the input
-
-    var body = data;
-    var params = {
-      object: file.name,
-      'x-amz-meta-customLabels': note_customtag, // Adding custom labels in metadata
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
-        "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
-      },
-      folder: "ass-b2"
+    reader.onerror = error => {
+      console.error("Error reading file:", error);
+      reject(error);
     };
-
-    var additionalParams = {};
-    apigClient
-      .uploadPut(params, body, additionalParams)
-      .then(function (res) {
-        if (res.status == 200) {
-          document.getElementById("upload_button").innerHTML = "Upload succeeded";
-          document.getElementById("upload_button").style.backgroundColor = "#499C55";
-          console.log(res["data"]);
-        }
-      })
-      .catch(() => {
-        document.getElementById("upload_button").innerHTML = "Upload failed";
-        document.getElementById("upload_button").style.backgroundColor = "#F54234";
-      });
   });
 }
-
-
-const dropArea = document.querySelector(".drop_box"),
-  button = dropArea.querySelector("button"),
-  dragText = dropArea.querySelector("header"),
-  input = dropArea.querySelector("input");
-let file;
-var filename;
-
-button.onclick = () => {
-  input.click();
-};
-
-input.addEventListener("change", function (e) {
-  var fileName = e.target.files[0].name;
-  let filedata = `
-    <h4>${fileName}</h4>
-    <input placeholder="Input Custom tag!" type="text" class="form-control" id="note_customtag">
-    <button class="btn" id="upload_button" type="submit" onclick="uploadPhoto()">Upload</button>
-    `;
-  file = document.getElementById("file_path").files[0];
-  console.log(file);
-  dropArea.innerHTML = filedata;
-});
